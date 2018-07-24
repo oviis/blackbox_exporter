3e1964850c9cf8193dd48c00ceff9eb963763cb6
Chase Sillevis <chase@sillevis.net>
2018-07-24 11:10:52 +0200

    Move away from prometheus

--- main.go
+++ main.go
@@ -42,8 +42,8 @@ import (
 	"gopkg.in/alecthomas/kingpin.v2"
 	"gopkg.in/yaml.v3"
 
-	"github.com/prometheus/blackbox_exporter/config"
-	"github.com/prometheus/blackbox_exporter/prober"
+	"github.com/DeviaVir/blackbox_exporter/config"
+	"github.com/DeviaVir/blackbox_exporter/prober"
 	dto "github.com/prometheus/client_model/go"
 )
 
